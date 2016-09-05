53b6c8f848dd428c01867f74a3268ed3c53ccd47
Matthijs Wilhelmus <mwilhelm@avans.nl>
2016-09-05 13:59:02 +0200

    Wrote intro comment about theme of project

---
+++ Metro 2033/src/metro/pkg2033/Metro2033.java
@@ -0,0 +1,26 @@
+package metro.pkg2033;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Matthijs Wilhelmus
+ * 
+ * 
+ * @version 1.1
+ */
+public class Metro2033 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+      //This java application will revolve around the story of the bestselling novel "Metro 2033".
+    }
+    
+}
